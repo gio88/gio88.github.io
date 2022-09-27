fe4b8935f8c7d6d1feb6ce14b35293d899da5739
gio88 <g.massa88@gmail.com>
2022-09-27 19:32:44 +0200

    Update Sitecore-webtags.js

--- Sitecore-WebTags.js
+++ Sitecore-WebTags.js
@@ -6,8 +6,8 @@ var _boxeverq = _boxeverq || [];
 // Define the Boxever settings 
 var _boxever_settings = {
 	client_key: 'psfu6uh05hsr9c34rptlr06dn864cqrx', // Replace with your client key
-	target: '{{apiTargetEndpoint}}', // Replace with your API target endpoint specific to your data center region
-	cookie_domain: '{{cookieDomain}}', // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
+	target: 'https://api.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
+	cookie_domain: '.github.io', // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
 	javascriptLibraryVersion: '{{javascriptLibraryVersion}}', // Replace with the latest Boxever JavaScript Library version"
 	pointOfSale: '{{pointOfSale}}', // Replace with the same point of sale configured in system settings"
 	web_flow_target: '{{web_flow_target}}', // Replace with path for the Amazon CloudFront CDN for Sitecore Personalize"
@@ -16,6 +16,6 @@ var _boxever_settings = {
 // Import the Boxever JavaScript Library asynchronously 
 (function() {
 	 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;  
-	 s.src = 'https://d1mj578wat5n4o.cloudfront.net/boxever-{{clientVersion}}.min.js';
+	 s.src = 'https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.9.min.js';
 	 var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
 })();
